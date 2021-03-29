7d41605103180f704affa02eecbd2f075c5f71ec
Christine Yang <yang.christine.95@gmail.com>
2021-03-29 10:59:51 -0400

    Create frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Altus Empire</h1>
+        <footer>
+          Copyright &copy; 2021 AE
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
---
+++ frontend/src/components/nav/navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './navbar.css'
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+      e.preventDefault();
+      this.props.logout();
+  }
+
+  // Selectively render links dependent on whether the user is logged in
+  getLinks() {
+      if (this.props.loggedIn) {
+        return (
+            <div>
+                <Link to={'/profile'}>Profile</Link>
+                <button onClick={this.logoutUser}>Logout</button>
+            </div>
+        );
+      } else {
+        return (
+            <div>
+                {/* Open modal pass in from props */}
+                <button onClick={() => openModal('login')}>Log in</button>
+                <button onClick={() => openModal('signup')}>Sign up</button>
+            </div>
+        );
+      }
+  }
+
+  render() {
+      return (
+        <div>
+            <h1>Altus Empire</h1>
+            { this.getLinks() }
+        </div>
+      );
+  }
+}
+
+export default NavBar;
