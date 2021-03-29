eaba74ea5cc423a0d0b424419d4de6dd7c83f103
Kevin <kevinmao.xy@gmail.com>
2021-03-29 09:35:17 -0400

    config mongoose

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require('./config/keys').mongoURI;
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
 
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("frontend/build"));
+//     app.get("/", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//     });
+// }
+
+mongoose
+    .connect(db, { userNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
