7a9bd863cf41f0ec61bee1eb04df2b214815ab54
Greta <gretahayes19@gmail.com>
2021-03-29 12:46:32 -0400

    Start map api

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
+import { Route } from "react-router-dom";
 
 import MainPage from './main/main_page';
+import MapContainer from './map/map';
 
 const App = () => (
     <div>
-        <Modal />
+        {/* <Modal /> */}
         <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
+            <Route path="/" component={MapContainer} />
         </Switch>
     </div>
 );
---
+++ frontend/src/components/map/map.js
@@ -0,0 +1,30 @@
+
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+};
+
+export class MapContainer extends Component {
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                    {
+                        lat: -1.2884,
+                        lng: 36.8233
+                    }
+                }
+            />
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDIEumsEBQJ1SBu1boGzGQUtYvi7Q2YSE4'
+})(MapContainer);
