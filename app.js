b675a2af711765761f9ffba215869480aea53159
Greta <gretahayes19@gmail.com>
2021-03-29 17:40:39 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -28,6 +28,4 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
-
-app.get("/", (req, res) => res.send("Hello World"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
