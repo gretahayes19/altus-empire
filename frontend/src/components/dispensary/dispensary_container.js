6098f68aecc6c798d564a49d3417f23f84684d1c
Greta <gretahayes19@gmail.com>
2021-03-30 18:30:40 -0400

    update dispensary container to fetch reviews

--- frontend/src/components/dispensary/dispensary_container.js
+++ frontend/src/components/dispensary/dispensary_container.js
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 import Dispensary from './dispensary';
+import {fetchReviews} from '../../actions/rating_actions'
 
 const mSTP = state => ({
     loggedIn: state.session.isAuthenticated,
     
 });
 
-// const mDTP = dispatch => ({
-
-// })
+const mDTP = dispatch => ({
+    fetchReviews: dispendaryId => dispatch(fetchReviews(dispendaryId))
+})
 
 export default connect(mSTP)(Dispensary);
