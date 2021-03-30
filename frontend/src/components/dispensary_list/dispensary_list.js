9373174d812195de43e394016c87f65f151af0db
Kevin <kevinmao.xy@gmail.com>
2021-03-30 15:02:28 -0400

    Add dispendary list container

---
+++ frontend/src/components/dispensary_list/dispensary_list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class DispensaryList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dispensaries: []
+        }
+    }
+}
+
+export default DispensaryList;
---
+++ frontend/src/components/dispensary_list/dispensary_list_container.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+
+const mSTP = state => ({
+    
+});
+
+const mDTP = dispatch => ({
+
+})
+
+export default connect(mSTP, mDTP)(NavBar);
