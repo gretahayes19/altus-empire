23ac0a2d58e3883a3fb28082e977a8de7236f8a5
yangc95 <72277593+yangc95@users.noreply.github.com>
2021-03-30 13:46:27 -0400

    style

--- routes/api/dispensary.js
+++ routes/api/dispensary.js
@@ -14,69 +14,72 @@ router.get('/all', (req, res) => {
         .catch(err => res.status(404).json(err))
 })
 
-router.post('/:id/photo', alowCrossDomain, [auth], async (req, res) => {
-    try {
-        const dispensary = await Dispensary.findById(req.params.id);
+// router.post('/:id/photo', allowCrossDomain, [auth], async (req, res) => {
+//     try {
+//         const dispensary = await Dispensary.findById(req.params.id);
 
-        if(!dispensary) {
-            return res.status(404).json({ msg: 'Dispensary not found' });
-        }
+//         if(!dispensary) {
+//             return res.status(404).json({ msg: 'Dispensary not found' });
+//         }
 
-        if (!req.files) {
-            res.status(400).json({
-                errors: [
-                    {
-                        msg: 'Please upload a file'
-                    }
-                ]
-            });
-        }
-        const file = req.files.file
+//         if (!req.files) {
+//             res.status(400).json({
+//                 errors: [
+//                     {
+//                         msg: 'Please upload a file'
+//                     }
+//                 ]
+//             });
+//         }
+//         const file = req.files.file
 
-        if (!file.mometype.startsWith('image')) {
-            res.status(400).json({
-                errors: [
-                    {
-                        msg: 'Images only'
-                    }
-                ]
-            });
-        }
+//         if (!file.mometype.startsWith('image')) {
+//             res.status(400).json({
+//                 errors: [
+//                     {
+//                         msg: 'Images only'
+//                     }
+//                 ]
+//             });
+//         }
 
-        file.name = `photo_${post._id}${path.parse(file.name).ext}`;
+//         file.name = `photo_${post._id}${path.parse(file.name).ext}`;
         
-        var Blob = req.files.file.data;
+//         var Blob = req.files.file.data;
 
-        const S3_BUCKET = 'mybucketname';
-        const AWS_ACCESS_KEY_ID = 'Enter here';
-        const AWS_SECRET_ACCESS_KEY = 'Enter Here';
+//         const S3_BUCKET = 'mybucketname';
+//         const AWS_ACCESS_KEY_ID = 'Enter here';
+//         const AWS_SECRET_ACCESS_KEY = 'Enter Here';
 
-        AWS.config.update({
-            accessKeyId: AWS_ACCESS_KEY_ID,
-            secretAccessKey: AWS_SECRET_ACCESS_KEY
-        });
+//         AWS.config.update({
+//             accessKeyId: AWS_ACCESS_KEY_ID,
+//             secretAccessKey: AWS_SECRET_ACCESS_KEY
+//         });
 
-        const s3 = new AWS.S3();
+//         const s3 = new AWS.S3();
 
-        var params = {
-            Bucket: S3_BUCKET,
-            Key: file.name,
-            Body: Blob
-        };
-        s3.upload(params, function(err, data) {
-            console.log(err, data);
-        });
+//         var params = {
+//             Bucket: S3_BUCKET,
+//             Key: file.name,
+//             Body: Blob
+//         };
+//         s3.upload(params, function(err, data) {
+//             console.log(err, data);
+//         });
         
-        let newPhoto = await Dispensary.findByIdAndUpdate(
-            req.params.id,
-            {
-                postphoto: file.name
-            },
-            { new: true }
-        );
+//         let newPhoto = await Dispensary.findByIdAndUpdate(
+//             req.params.id,
+//             {
+//                 postphoto: file.name
+//             },
+//             { new: true }
+//         );
         
-        res.json(newPhoto);
-    }
-});
+//         res.json(newPhoto);
+//     } catch (err) {
+//         // is this the correct return?
+//         return res.status(404).json(err)
+//     }
+// });
 
 module.exports = router;
