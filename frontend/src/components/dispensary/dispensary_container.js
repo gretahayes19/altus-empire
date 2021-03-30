2a0e3dbe350246e67d99c089e8ae8f32dda0c19b
Greta <gretahayes19@gmail.com>
2021-03-30 18:35:53 -0400

    reviews reducer

--- frontend/src/components/dispensary/dispensary_container.js
+++ frontend/src/components/dispensary/dispensary_container.js
@@ -2,8 +2,10 @@ import { connect } from 'react-redux';
 import Dispensary from './dispensary';
 import {fetchReviews} from '../../actions/rating_actions'
 
-const mSTP = state => ({
+const mSTP = (state, ownProps) => ({
     loggedIn: state.session.isAuthenticated,
+    dispensaryId: ownProps.dispensaryId,
+    reviews: state.entities.reviews,
     
 });
 
