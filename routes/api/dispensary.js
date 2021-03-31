61fd6e5c45ddd7fddfdbbfaf9fc417edf8bf2df0
Hank <hankchen1997@yahoo.com>
2021-03-30 21:41:09 -0400

    d

--- routes/api/dispensary.js
+++ routes/api/dispensary.js
@@ -7,12 +7,68 @@ const passport = require('passport');
 const validateDispensaryInput = require('../../validation/dispensary')
 require('../../config/passport.js')(passport)
 
+// AWS
+require('dotenv').config()
+const multer = require('multer')
+let AWS = require('aws-sdk')
+
+let storage = multer.memoryStorage()
+let upload = multer({ storage: storage })
+
 router.get('/all', (req, res) => {
     Dispensary
         .find({})
         .then((dispensaries) => res.json(dispensaries))
         .catch(err => res.status(404).json(err))
 })
+
+const AWS_FILE_URL_LINK = ""
+const AWS_ACCESS_KEY_ID = ""
+const AWS_SECRET_ACCESS_KEY = ""
+const AWS_BUCKET_NAME = ""
+
+
+// router.post("/upload", upload.single('file'), function(req, res) {
+//     const file = req.file
+//     const awsFileURL = process.env.AWS_FILE_URL_LINK
+
+//     let s3bucket = new AWS.S3({
+//         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//         region: process.env.AWS_REGION,
+//     })
+
+//     let params = {
+//         Bucket: process.env.AWS_BUCKET_NAME,
+//         Key: file.originalname,
+//         Body: file.buffer,
+//         ContentType: file.mimetype,
+//         ACL: "public-read",
+//     }
+
+//     s3bucket.upload(params, function (err, data) {
+//         if (err) {
+//         res.status(500).json({ error: true, Message: err });
+//         } else {
+//         res.send({ data });
+        
+//         let newFileUploaded = {
+//                 post_id: req.body.post_id,
+//                 description: req.body.description,
+//                 fileLink: s3FileURL + file.originalname,
+//                 s3_key: params.Key,
+//         };
+        
+//         let photo = new PHOTO(newFileUploaded);
+//             photo.save(function (error, newFile) {
+//                     if (error) {
+//                         throw error;
+//                     }
+//             })
+//         }
+//     })
+// })
+
 
 // router.post('/:id/photo', allowCrossDomain, [auth], async (req, res) => {
 //     try {
