8b9ba880f00ec8cc9fd5d13745a0699697eedfe6
hank chen <hankchen@hanks-MacBook-Pro.local>
2021-03-31 17:11:26 -0400

    fixed keys_dev_password"

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,9 +1,5 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
-    AWS_FILE_URL_LINK: "https://s3-us-east-1.amazonaws.com/altus-empire-seeds/",
-    AWS_ACCESS_KEY_ID: "AKIA3KJF7TLUJCRCFEP4",
-    AWS_SECRET_ACCESS_KEY: "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",
-    AWS_BUCKET_NAME: "altus-empire-seeds",
-    AWS_REGION: "us-east-1"
+  
 }
