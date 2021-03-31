aeb124641e7316d314925f8fd8f476c67181f77a
Greta <gretahayes19@gmail.com>
2021-03-30 20:17:23 -0400

    dispensary reviews

--- frontend/src/components/dispensary/dispensary.js
+++ frontend/src/components/dispensary/dispensary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReviewIndex from './review_index'
 
 class Dispensary extends React.Component {
   constructor(props) {
@@ -6,16 +7,20 @@ class Dispensary extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchReviews(this.props.match.params.dispensaryId)
+    debugger
+    this.props.fetchReviews(this.props.match.params.dispensaryId);
+    this.props.fetchDispensaries();
   }
 
   render() {
-    if (!this.props.review) return null;
+    if (!this.props.reviews) return null;
+    if (!this.props.dispensary) return null;
 
-
+    const {reviews, dispensary } = this.props 
       return (
           <div>
-              
+              <h1>{dispensary.dispensaryName}</h1>
+             <ReviewIndex reviews={reviews} />
           </div>
       )
   }
--- frontend/src/components/dispensary/dispensary_container.js
+++ frontend/src/components/dispensary/dispensary_container.js
@@ -1,16 +1,23 @@
 import { connect } from 'react-redux';
 import Dispensary from './dispensary';
 import {fetchReviews} from '../../actions/rating_actions'
+import { fetchDispensaries } from '../../actions/dispensary_actions';
 
-const mSTP = (state, ownProps) => ({
+
+const mSTP = (state, ownProps) => {
+    debugger
+    return {
     loggedIn: state.session.isAuthenticated,
-    dispensaryId: ownProps.dispensaryId,
-    reviews: state.entities.reviews,
-    
-});
+    dispensary: state.entities.dispensaries[ownProps.match.params.dispendaryId],
+    dispensaryId: ownProps.match.params.dispensaryId,
+    reviews: state.entities.reviews,   
+    }
+};
 
 const mDTP = dispatch => ({
-    fetchReviews: dispendaryId => dispatch(fetchReviews(dispendaryId))
+    fetchReviews: dispendaryId => dispatch(fetchReviews(dispendaryId)),
+    fetchDispensaries: () => dispatch(fetchDispensaries())
+
 })
 
-export default connect(mSTP)(Dispensary);
+export default connect(mSTP, mDTP)(Dispensary);
