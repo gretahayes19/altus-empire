ad9f6ec9c7456ce559ff63cdabc7d3dd12e9bb41
Kevin <kevinmao.xy@gmail.com>
2021-03-30 21:15:21 -0400

    Fix bug on dispensary state

--- frontend/src/components/dispensary/dispensary.js
+++ frontend/src/components/dispensary/dispensary.js
@@ -4,22 +4,34 @@ import ReviewIndex from './review_index'
 class Dispensary extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      reviews: [],
+      dispensary: {}
+    }
   }
 
   componentDidMount() {
-    debugger
-    this.props.fetchReviews(this.props.match.params.dispensaryId);
-    this.props.fetchDispensaries();
+    this.props.fetchReviews(this.props.match.params.dispensaryId).then(
+      this.props.fetchDispensaries().then(
+        () => this.setState({reviews: this.props.reviews, dispensary: this.props.dispensary})
+      )
+    );
+    
+  }
+
+  componentDidUpdate() {
+    console.log(this.state)
   }
 
   render() {
-    if (!this.props.reviews) return null;
+    // if (!this.state.reviews.length) return null;
     if (!this.props.dispensary) return null;
 
-    const {reviews, dispensary } = this.props 
+    const {reviews, dispensary } = this.state 
       return (
           <div>
               <h1>{dispensary.dispensaryName}</h1>
+              <h1>Hello</h1>
              <ReviewIndex reviews={reviews} />
           </div>
       )
--- frontend/src/components/dispensary/dispensary_container.js
+++ frontend/src/components/dispensary/dispensary_container.js
@@ -3,12 +3,18 @@ import Dispensary from './dispensary';
 import {fetchReviews} from '../../actions/rating_actions'
 import { fetchDispensaries } from '../../actions/dispensary_actions';
 
+const formatArrToObj = arr => {
+    let obj = {};
+    arr.forEach(ele => obj[ele._id] = ele)
+    console.log(obj);
+    return obj;
+}
 
 const mSTP = (state, ownProps) => {
-    debugger
+    // debugger
     return {
     loggedIn: state.session.isAuthenticated,
-    dispensary: state.entities.dispensaries[ownProps.match.params.dispendaryId],
+    dispensary: formatArrToObj(state.entities.dispensaries)[ownProps.match.params.dispensaryId],
     dispensaryId: ownProps.match.params.dispensaryId,
     reviews: state.entities.reviews,   
     }
