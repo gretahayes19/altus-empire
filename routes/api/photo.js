260d71d321423800d9de9423126780b8dfc2bc41
Hank <hankchen1997@yahoo.com>
2021-04-01 15:54:49 -0400

    added seeds to disp and aws photos

--- routes/api/photo.js
+++ routes/api/photo.js
@@ -5,37 +5,34 @@ require('../../config/passport.js')(passport)
 
 // // AWS
 // // require('dotenv').config()
-// const multer = require('multer')
-// let AWS = require('aws-sdk')
-// const Photo = require('../../models/Photo')
+const multer = require('multer')
+let AWS = require('aws-sdk')
+const Photo = require('../../models/Photo')
 
-// let storage = multer.memoryStorage()
-// let upload = multer({ storage: storage })
+let storage = multer.memoryStorage()
+let upload = multer({ storage: storage })
 
 const keys = require('../../config/keys');
 
+router.get('/test', (req, res) => res.json({msg: "photo route connected"}))
 
+router.post("/upload", upload.single('file'), function(req, res) {
+    const file = req.file
+    const awsFileURL = keys.AWS_FILE_URL_LINK
 
-// router.get('/test', (req, res) => res.json({msg: "photo route connected"}))
+    let s3bucket = new AWS.S3({
+        accessKeyId: keys.AWS_ACCESS_KEY_ID,
+        secretAccessKey: keys.AWS_SECRET_ACCESS_KEY,
+        region:  keys.AWS_REGION,
+    })
 
-// router.post("/upload", upload.single('file'), function(req, res) {
-//     const file = req.file
-//     const awsFileURL = keys.AWS_FILE_URL_LINK
-
-//     let s3bucket = new AWS.S3({
-//         accessKeyId: keys.AWS_ACCESS_KEY_ID,
-//         secretAccessKey: keys.AWS_SECRET_ACCESS_KEY,
-//         region:  keys.AWS_REGION,
-//     })
-
-
-//     let params = {
-    //         Bucket: keys.AWS_BUCKET_NAME,
-    //         Key: file.originalname,
-    //         Body: file.buffer,
-    //         ContentType: file.mimetype,
-    //         ACL: "public-read",
-    //     }
+    let params = {
+            Bucket: keys.AWS_BUCKET_NAME,
+            Key: file.originalname,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+            ACL: "public-read",
+        }
 
     // let dispensary = Dispensary.findOne(_id: dispensary.id)
     // if (!dispensary) {
@@ -44,28 +41,26 @@ const keys = require('../../config/keys');
     //   Dispensary.updateOne(_id: dispensary.id);
     // }
 
-//     s3bucket.upload(params, function (err, data) {
-//         if (err) {
-//         res.status(500).json({ error: true, Message: err });
-//         } else {
-//         res.send({ data });
+    s3bucket.upload(params, function (err, data) {
+        if (err) {
+        res.status(500).json({ error: true, Message: err });
+        } else {
+        res.send({ data });
         
-//         let newFileUploaded = {
-//                 postId: req.body.post_id,
-//                 fileLink: awsFileURL + file.originalname,
-//                 s3Key: params.Key,
-//         };
+        let newFileUploaded = {
+                postId: req.body.post_id,
+                fileLink: awsFileURL + file.originalname,
+                s3Key: params.Key,
+        };
         
-//         let photo = new Photo(newFileUploaded);
-//             photo.save(function (error, newFile) {
-//                     if (error) {
-//                         throw error;
-//                     }
-//             })
-//         }
-
-
-//     })
-// })
+        let photo = new Photo(newFileUploaded);
+            photo.save(function (error, newFile) {
+                    if (error) {
+                        throw error;
+                    }
+            })
+        }
+    })
+})
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -13,15 +13,14 @@ seeder.connect(db, function() {
         })
     })
 
-    seeder.clearModels(['rating'], function() {
-        seeder.populateModels(ratingData, function() {
-            seeder.disconnect()
-        })
-    })
+    // seeder.clearModels(['rating'], function() {
+    //     seeder.populateModels(ratingData, function() {
+    //         seeder.disconnect()
+    //     })
+    // })
 })
 
 const dispensaryData = [
-
     {
         'model' : 'dispensary',
         'documents' : [
@@ -34,6 +33,7 @@ const dispensaryData = [
                 phone: 9177931107,
                 address: '178 N 4th St Brooklyn, NY 11211',
                 website: 'https://www.sunnyside.shop/',
+                postphoto: "sunnyside.jpg",
                 avgRating: 4.5
             },
             {
@@ -45,6 +45,7 @@ const dispensaryData = [
                 phone: 2122710925,
                 address: '44 Court St Brooklyn, NY 11201',
                 website: 'https://col-care.com/',
+                postphoto: "bestores.jpeg",
                 avgRating: 3.8
             },
             {
@@ -56,6 +57,7 @@ const dispensaryData = [
                 phone: 6464537178,
                 address: '212 E 14th St New York, NY 10003',
                 website: 'https://col-care.com/',
+                postphoto: "colcare.jpg",
                 avgRating: 3.0
             },
             {
@@ -67,7 +69,96 @@ const dispensaryData = [
                 phone: 2122573145,
                 address: '202 Flatbush Ave Brooklyn, NY 11217',
                 website: 'https://bestores.com/',
+                postphoto: "colcareny.jpeg",
                 avgRating: 5.0
+            },
+            {
+                dispensaryName: 'Vireo Health',
+                description: 'Vireo Health aims to provide medicinal marijuana to New York residents with a NY medical marijuana card—minus the hassle. Staffed by a knowledgeable team of physicians, pharmacists, scientists, and horticulturalists, Vireo Health shares its wealth of expertise of cannabis-based remedies with patients, all while ensuring that each transaction abides by current New York regulations.',
+                latitude: 40.738361,
+                longitude: -73.886108,
+                phone: 8444847366,
+                address: '89-55 Queens Blvd, Queens, NY 11373',
+                website: 'https://vireohealth.com/ny/dispensaries/queens/',
+                postphoto: "VireoHealth.jpg",
+                avgRating: 4.4
+            },
+            {
+                dispensaryName: 'Curaleaf NY Queens',
+                description: 'Welcome to our home. Where the grass is not just greener, it’s better. Some people walk in knowing what they’re looking for— a tincture to ease the pain or perhaps a flower to make the day brighter. Then there are those who walk in curious, ready to start their journey with this ancient plant.',
+                latitude: 40.72097,
+                longitude: -73.84521,
+                phone: 6469682872,
+                address: '10718 70th Rd, Queens, NY 11375',
+                website: 'https://curaleaf.com/',
+                postphoto: "curaleaf.jpg",
+                avgRating: 3
+            },
+            {
+                dispensaryName: 'The Botanist',
+                description: 'Our approach to patient care is attentive and detailed. Every patient and their registered caregivers complete an in-depth consultation with our highly-educated on-site pharmacists. Once we are familiar with our patients’ qualified medical conditions, they are then referred to our knowledgeable dispensary technicians who help them find the right cannabis-based remedies for their symptoms.',
+                latitude: 40.703869,
+                longitude: -73.815231,
+                phone: 9178104540,
+                address: '138-72 Queens Blvd, Jamaica, NY 11435',
+                website: 'https://www.shopbotanist.com/locations/queens-medical-marijuana/',
+                postphoto: "theBotanist.jpg",
+                avgRating: 4.8
+            },
+            {
+                dispensaryName: 'Veriflife Medical Marijuana Dispensary ',
+                description: 'Your story is our story. In fact, we made it our purpose. Verilife believes that everyone should have access to the best possible care. From the products we curate to the personalized customer care we provide, we put our belief into action with truth and transparency. This is our purpose and its only natural that we find ourselves here—showing the many ways marijuana can, and does, improve quality of life.',
+                latitude: 40.80963,
+                longitude: -73.88116,
+                phone: 7188422001,
+                address: '405 Hunts Point Ave, The Bronx, NY 10474',
+                website: 'https://www.verilife.com/ny/location/bronx?utm_source=google&utm_medium=organic&utm_campaign=bronx_gmb_location',
+                postphoto: "verilife.jpg",
+                avgRating: 4.8
+            },
+            {
+                dispensaryName: 'Etain Health',
+                description: 'A major figure in Irish mythology (and the namesake of our company), Etain was an unrivaled beauty whose story is one of perseverance and transformation. Like our Granny Franny, she reminds us that no matter the challenges life brings, an enduring spirit will always win out in the end—even if in a new, unexpected form.',
+                latitude: 40.76022,
+                longitude: -73.96537,
+                phone: 9144377898,
+                address: '242 E 58th St, New York, NY 10022',
+                website: 'https://etainhealth.com/',
+                postphoto: "EtainHealth.jpg",
+                avgRating: 3.9
+            },
+            {
+                dispensaryName: 'Hemped NYC',
+                description: 'Our story began in Boulder, Colorado on March 3, 2014. This was our first attempt at a harvest. For months and months we began cultivating and harvesting, harvesting and cultivating. The outcome was not what we were hoping for. Although we were getting good at harvesting the hemp flower, It seemed to be very meticulous work. And as opposed to the extensive knowledge and track record of harvesting the Marijuana plant, there was much less information out there on growing the hemp leaf. Both Marijuana and Hemp, are indeed from the same plant species, but quit different in the way that they are harvested and cultivated.',
+                latitude: 40.7309461,
+                longitude: -74.0026585,
+                phone: 8444436733,
+                address: '247 Bleecker St, New York, NY 10014',
+                website: 'http://www.hempednyc.com/',
+                postphoto: "hempedNYC.jpg",
+                avgRating: 3.9
+            },
+            {
+                dispensaryName: 'CBD Oil New York',
+                description: 'CBDPure is a grower and producer of high-quality full-spectrum cannabidiol ("CBD") oil derived from certified organic-standards industrial hemp grown in Colorado and Washington. Our line of CBD products are all made from hemp grown locally, without the use of harmful pesticides and herbicides. The genetics of the industrial hemp plant we use has been chosen for its high CBD content and is grown with great care in order to produce a high quality product you can count on, that is tested and certified by third-party labs for every batch.',
+                latitude: 40.7042695,
+                longitude: -74.0117256,
+                phone: 9177281649,
+                address: '80 Broad St suite 950, New York, NY 10004',
+                website: 'https://www.cbdpure.com/?AFFID=451362',
+                postphoto: "CBDPURE.jpg",
+                avgRating: 4.5
+            },
+            {
+                dispensaryName: 'Medical Marijuana Consolutations of NYC',
+                description: 'Our Right NY is a full service Medical Marijuana Consultation Clinic. We strive to make obtaining a Medical Marijuana License as simple and discreet as possible. Our Representatives and Doctors have many years in the field and a vast knowledge in all aspects of Medical Marijuana.',
+                latitude: 40.8104684,
+                longitude: -73.946384,
+                phone: 3478613009,
+                address: '164 W 128th St #4d, New York, NY 10027',
+                website: 'https://ourrightny.com/',
+                postphoto: "MMconsolutantNYC.jpg",
+                avgRating: 4.5
             },
         ]
     }
