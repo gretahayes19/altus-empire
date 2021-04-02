54fc3c7a8080a6cf49b9032f270e9a36d26c592b
Kevin <kevinmao.xy@gmail.com>
2021-04-02 11:34:32 -0400

    What d

--- routes/api/users.js
+++ routes/api/users.js
@@ -34,7 +34,7 @@ router.post('/register', (req, res) => {
 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;d
+                        if (err) throw err;
                         newUser.password = hash;
                         newUser.save()
                             .then(user => {
